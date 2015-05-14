a49220493f1177ae0ecc3e360b55243e815c4826
Francisco Roche <elcisko@gmail.com>
2015-05-13 23:37:51 -0430

    ajuste menor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,13 +80,4 @@ Rails.application.configure do
   # Note to set this to your host
   config.action_mailer.default_url_options = { :host => 'omr-pisteresting.herokuapp.com' }
 
-  # creado para paperclip subir imagenes a Amazon S3
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
 end
